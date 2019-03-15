403835387843dd548938ac45702cfbe4dd489369
Uriel Hayon <uri.a77aque@gmail.com>
2019-03-15 08:51:40 -0300

    SEO

--- src/components/Footer/ContactForm/ContactForm.js
+++ src/components/Footer/ContactForm/ContactForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropsTypes from 'prop-types';
-import ReactGa from 'react-ga';
+import ReactGA from 'react-ga';
 
 import FormField from './FormField/FormField';
 
@@ -41,11 +41,11 @@ class ContactForm extends React.Component {
       sendingMail: true,
       sendingMailResponse: ''
     }), () => {
-      ReactGa.event({
+      ReactGA.event({
         category: 'Contact',
         action: 'Sent a message through the contact form'
       });
-      
+
       const { sendingMail, sendingMailResponse, ...contacyBody} = this.state;
       fetch('https://ur-portfolio-api.herokuapp.com/contact', {
         method: 'post',
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactGa from 'react-ga';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactGa.initialize('UA-136305497-1');
+ReactGA.initialize('UA-136305497-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
